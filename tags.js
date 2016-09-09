eea066313ab56340cea151ca7396f75b060cdf9d
lisunovdv <lisunovdv@gmail.com>
2016-09-09 18:09:30 +0300

    first commit

---
+++ tags.js
@@ -0,0 +1,14 @@
+/**
+ *! Created by admin on 07.09.2016.
+ */
+
+function TagModule(name) {
+    this.closed = ['textarea', 'button', 'div'];
+    this.unclosed = ['input', 'br', 'hr'];
+
+    this.stringTagName = name;
+    this.isUnclosed = function () {
+        if (.indexOf("Apple");)
+    }
+}
+
